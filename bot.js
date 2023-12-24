01fae16b2fc77acca15d871ae796b38c630b56fe
Igor Gotovchik <gotovchik.igor@yandex.ru>
2023-12-24 15:44:07 +0300

    first command

--- bot.js
+++ bot.js
@@ -1,28 +1,28 @@
 "use strict";
 
-
-// import { ConfigService } from "./src/config/ConfigService.js";
+import { ConfigService } from "./src/config/ConfigService.js";
 import { Movie } from "./src/movie.js";
-// import { Bot } from "grammy";
+import { Bot } from "grammy";
 
-// const cs = new ConfigService();
-// const bot = new Bot(`${cs.get("BOT_TOKEN")}`);
-
-
-
-// bot.command("start", (ctx) => {
-//   ctx.reply("Hello!");
-// });
-const showData = async () => {
-  const qb = new Movie();
-  const data = await qb.getRandomMovieWithFilters();
-  console.log(data);
-};
-
-showData();
+const cs = new ConfigService();
+const bot = new Bot(`${cs.get("BOT_TOKEN")}`);
+const movie = new Movie();
 
 
-// bot.start();
+bot.command("start", (ctx) => {
+  ctx.reply("Hello!");
+});
+
+bot.hears("Мне повезёт!", async (ctx) => {
+  const data = await movie.getRandomMovieWithFilters();
+  const string = `Ваш случайный фильм:\nНазвание: ${data.name} | Год: ${data.year}\nРейтинг IMDB: ${data.rating.imdb}\n\nОписание:\n${data.description}`;
+  const posterUrl = data.poster.previewUrl;
+  await ctx.reply(string);
+  await ctx.replyWithPhoto(posterUrl);
+});
+
+
+bot.start();
 
 
 
--- src/movie.js
+++ src/movie.js
@@ -6,65 +6,27 @@ const { KinopoiskDev, MovieQueryBuilder, SPECIAL_VALUE } = pkg;
 
 export class Movie {
 
-  // getRandomMovieWithFilters = async () => {
-  //   const cs = new ConfigService();
-  //   const kp = new KinopoiskDev(cs.get("KP_TOKEN"))
-  //   const queryBuilder = new MovieQueryBuilder();
-
-  //   const baseQuery = queryBuilder
-  //     .select([`name`, `year`])
-  //     .filterRange(`year`, [2010, 2023])
-  //     .filterRange(`rating.kp`, [6, 10])
-  //     .filterRange(`typeNumber`, [1])
-  //     .filterExact(`genres.name`, [`для взрослых`, 'короткометражка']);
-
-  //   const firstQuery = baseQuery
-  //     .paginate(1, 1)
-  //     .build();
-
-  //   const firstRes = await kp.movie.getByFilters(firstQuery);
-
-  //   if (firstRes.data) {
-  //     const { pages } = firstRes.data;
-
-  //     const randomPage = Math.floor(Math.random() * pages) + 1;
-  //     const query = baseQuery.paginate(randomPage, 1).build();
-
-  //     const { data, error, message } = await kp.movie.getByFilters(query);
-
-  //     if (data) {
-  //       return data.docs[0];
-  //     }
-  //     if (error) console.log(error, message);
-  //   }
-  //   if (firstRes.error) console.log(firstRes.error, firstRes.message);
-  // };
-
   getRandomMovieWithFilters = async () => {
     const cs = new ConfigService();
     const kp = new KinopoiskDev(cs.get("KP_TOKEN"))
-    // Создаем билдер запросов для фильмов
+
     const queryBuilder = new MovieQueryBuilder();
 
-    // Выбираем поля, которые мы хотим получить в ответе
     const baseQuery = queryBuilder
-      .select(['id', 'name', 'rating', 'poster', 'year'])
-      // Добавляем фильтр для поиска фильмов с постером
+      .select(['id', 'name', 'rating', 'poster', 'year', 'description'])
       .filterExact('poster.url', SPECIAL_VALUE.NOT_NULL)
-      // Добавляем фильтр для поиска фильмов с трейлерами
-      .filterExact('videos.trailers.url', SPECIAL_VALUE.NOT_NULL);
+      .filterExact('videos.trailers.url', SPECIAL_VALUE.NOT_NULL)
+      .filterExact('rating.imdb', SPECIAL_VALUE.NOT_NULL)
+      .filterExact('description', SPECIAL_VALUE.NOT_NULL)
+      .filterExact('name', SPECIAL_VALUE.NOT_NULL);
 
     const firstQuery = baseQuery
-      // получим первую страницу, чтобы узнать сколько всего фильмов удовлетворяет нашим фильтрам
       .paginate(1, 1)
-      // Собираем запрос
       .build();
 
-    // Отправляем запрос на получение количества фильмов
     const firstRes = await kp.movie.getByFilters(firstQuery);
     if (firstRes.data) {
       const { pages } = firstRes.data;
-      // Генерируем случайное число от 1 до pages
       const randomPage = Math.floor(Math.random() * pages) + 1;
       const query = baseQuery.paginate(randomPage, 1).build();
 
@@ -73,11 +35,9 @@ export class Movie {
       if (data) {
         return data.docs[0];
       }
-      // Если будет ошибка, то выведем ее в консоль
       if (error) console.log(error, message);
     }
 
-    // Если будет ошибка, то выведем ее в консоль
     if (firstRes.error) console.log(firstRes.error, firstRes.message);
   };
 }
